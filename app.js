7e7f4d1dc97223394a0319d6158683c91f772628
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-12 20:33:27 -0600

    add route arguments
    add routes and funnel response through multiple middleware

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((request, response, next) => {
-  console.log("Greetings, from inside the middleware");
+app.use("/", (request, response, next) => {
+  console.log(`This is Neo. Greetings.
+  Matrix 4 premiers in theatres (we hope -- I'm looking at you COVID-19) 
+  April, 2022. I'll be the guy handing out red pills. Hope to see you there. Bottoms up.`);
   next();
 });
 
-app.use((request, response, next) => {
-  console.log("Greetings, from inside the middleware... do you hear an echo?");
-  response.send("<h1>OMG Elon Mush!!! oops, I meant Musk. Elon <em>Musk</em>.</h1>")
+app.use("/spacex", (request, response, next) => {
+  response.send("<h1>OMG Elon Mush!!! oops, I meant Musk. Elon <em>Musk</em>.</h1>");
+});
+
+app.use("/", (request, response, next) => {
+  response.send("<h2>why would you come to the void?</h2>");
 });
 
 app.listen(3000);
