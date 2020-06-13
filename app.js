c77118029da8a47e83fac223b85491175720faa9
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-12 19:52:55 -0600

    illustrate the basic concept of middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((request, response, next) => {
+  console.log("Greetings, from inside the middleware");
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log("Greetings, from inside the middleware... do you hear an echo?");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
