7afaf09f8c16beccf8b6d06046d0c9d4ced90f60
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-12 19:59:52 -0600

    send a response using express

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use((request, response, next) => {
 
 app.use((request, response, next) => {
   console.log("Greetings, from inside the middleware... do you hear an echo?");
+  response.send("<h1>OMG Elon Mush!!! oops, I meant Musk. Elon <em>Musk</em>.</h1>")
 });
 
 const server = http.createServer(app);
