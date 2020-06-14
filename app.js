7457c13869ff5381c8ed5022c14619631018877b
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-13 23:06:20 -0600

    add /song route
    /song only acts on POST requests and redirects to /

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/", (request, response, next) => {
-  console.log(`This is Neo. Greetings.
-  Matrix 4 premiers in theatres (we hope -- I'm looking at you COVID-19) 
-  April, 2022. I'll be the guy handing out red pills. Hope to see you there. Bottoms up.`);
-  next();
-});
+// app.use("/", (request, response, next) => {
+//   console.log(`This is Neo. Greetings.
+//   Matrix 4 premiers in theatres (we hope -- I'm looking at you COVID-19) 
+//   April, 2022. I'll be the guy handing out red pills. Hope to see you there. Bottoms up.`);
+//   next();
+// });
 
 app.use("/spacex", (request, response, next) => {
   response.send("<h1>OMG Elon Mush!!! oops, I meant Musk. Elon <em>Musk</em>.</h1>");
@@ -18,6 +18,11 @@ app.use("/spacex", (request, response, next) => {
 
 app.use("/add-song", (request, response, next) => {
   response.send("<form action='/song' method='POST'><input type='text' name='title' placeholder='song title here'><button type='submit'>Add</button></form>");
+})
+
+app.post("/song", (request, response, next) => {
+  console.log(request.body.title);
+  response.redirect("/");
 })
 
 app.use("/", (request, response, next) => {
