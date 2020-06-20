c50dd70b281062a9c5120b3d9a21d4f00919d27a
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-19 22:57:53 -0600

    update route paths

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/add-song", (request, response, next) => {
-  response.sendFile(path.join(__dirname, "../", "views", "admin.html"));
+  response.sendFile(path.join(__dirname, "..", "views", "admin.html"));
 });
 
 router.post("/song", (request, response, next) => {
