0e9314d16b4d157ba3dfc9232201f0a61e0821e2
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-20 23:08:33 -0600

    update paths to html files
    refactor paths to html files sent with responses

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
+const rootDir = require('../helpers/path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get("/add-song", (request, response, next) => {
-  response.sendFile(path.join(__dirname, "..", "views", "admin.html"));
+  response.sendFile(path.join(rootDir, "views", "admin.html"));
 });
 
 router.post("/song", (request, response, next) => {
--- routes/player.js
+++ routes/player.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
+const rootDir = require('../helpers/path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get("/", (request, response, next) => {
-  response.sendFile(path.join(__dirname, "../", "views", "player.html"))
+  response.sendFile(path.join(rootDir, "views", "player.html"))
 });
 
 module.exports = router;
