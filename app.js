4b6984ce22f1e0ca9d9862d3fdc85377145b8c7f
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-26 20:17:08 -0600

    migrate styles to css files
    create admin.css and 404.css to house unique styles for respective html pages

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const rootDir = require('./helpers/path');
 const adminRoutes = require('./routes/admin');
 const playerRoutes = require('./routes/player');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(playerRoutes);
 
 app.use("/", (request, response, next) => {
-  response.status(404).sendFile(path.join(__dirname, "./", "views", "not-found.html"));
+  response.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
